1c0346ce37892117fe06c50dd5456add2752c2bc
ronaldheft-usds <52176593+ronaldheft-usds@users.noreply.github.com>
2022-02-10 17:20:10 +0000

    Deploying to gh-pages from @ usds/sba-direct-loan-prototype@e4e4144a61813118ef9f4f93dc6fc812e1a7e3cd 🚀

--- assets/js/inputs.js
+++ assets/js/inputs.js
@@ -5,3 +5,14 @@ function conditionalOption(id, show) {
         document.getElementById(id).classList.add('display-none');
     }
 }
+
+function checkEligibility() {
+    var businessOwnFalse = document.getElementById('business-own_option_1').checked;
+    var businessUsaFalse = document.getElementById('business-usa_option_1').checked;
+
+    if ( businessOwnFalse || businessUsaFalse ) {
+        window.location.href = 'eligibility-option-1-not-eligible.html';
+    } else {
+        window.location.href = 'eligibility-option-1-eligible.html';
+    }
+}
